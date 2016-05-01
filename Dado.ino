cf7732fa286efc642cc01a1ca44e6918ca7aca6a
Nelson Seixas <nelson1seixas@gmail.com>
2016-05-01 13:23:05 +0100

    Added files via upload

---
+++ Dado.ino
@@ -0,0 +1,52 @@
+int pinLeds1 = 10;
+int pinLeds2 = 9;
+int pinLeds3 = 7;
+int buttonPin = 6;
+int buttonState;
+long ran;
+int time = 2000;
+void setup ()
+{
+  pinMode (pinLeds1, OUTPUT);
+  pinMode (pinLeds2, OUTPUT);
+  pinMode (pinLeds3, OUTPUT);
+  pinMode (buttonPin, INPUT);
+  randomSeed(analogRead(0));
+}
+void loop()
+{
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH){
+    ran = random(1, 7);
+    if (ran == 1){
+      digitalWrite (pinLeds1, HIGH);
+      delay (time);
+    }
+    if (ran == 2){
+      digitalWrite (pinLeds3, HIGH);
+      delay (time);
+    }
+    if (ran == 3){
+      digitalWrite (pinLeds1, HIGH);
+      digitalWrite (pinLeds3, HIGH);
+      delay (time);
+    }
+    if (ran == 4){
+      digitalWrite (pinLeds2, HIGH);
+      delay (time);
+    }
+    if (ran == 5){
+      digitalWrite (pinLeds1, HIGH);
+      digitalWrite (pinLeds2, HIGH);
+      delay (time);
+   }
+   if (ran == 6){
+      digitalWrite (pinLeds2, HIGH);
+      digitalWrite (pinLeds3, HIGH);
+      delay (time);
+   }
+  }
+  digitalWrite (pinLeds1, LOW);
+  digitalWrite (pinLeds2, LOW);
+  digitalWrite (pinLeds3, LOW);
+}
